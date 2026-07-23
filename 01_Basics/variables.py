a21bdefb587babc4dd6b301bcc9acf846755dd6d
Kunal Jha <jhakunal817@gmail.com>
2026-07-23 07:53:46 +0545

    Refactor variable definitions with comments
    Consolidate variable declarations and add comments for clarity.

--- 01_Basics/variables.py
+++ 01_Basics/variables.py
@@ -1,16 +1,9 @@
 # variables.py
 
-# Integer
-age = 17
-
-# Float
-height = 5.8
-
-# String
-name = "Kunal"
-
-# Boolean
-is_student = True
+age = 17 #Integer Data Type
+height = 5.8 #Floating Data Type
+name = "Kunal" #String Data Type
+is_student = True #Boolean Data Type(Either true or false)
 
 print("Name:", name)
 print("Age:", age)
@@ -27,3 +20,13 @@ x, y, z = 10, 20, 30
 print("x =", x)
 print("y =", y)
 print("z =", z)
+
+#Output:
+# Name: Kunal
+# Age: 17
+# Height: 5.8
+# Student: True
+# Updated Age: 19
+# x = 10
+# y = 20
+# z = 30
