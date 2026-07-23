d9607265df503a46ef8768b19b25dee0c78fd0ec
Kunal Jha <jhakunal817@gmail.com>
2026-07-23 07:48:13 +0545

    Add variable examples and print statements

--- 01_Basics/variables.py
+++ 01_Basics/variables.py
@@ -1,1 +1,29 @@
+# variables.py
 
+# Integer
+age = 17
+
+# Float
+height = 5.8
+
+# String
+name = "Kunal"
+
+# Boolean
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+# Updating variables
+age = 19
+print("Updated Age:", age)
+
+# Multiple assignments
+x, y, z = 10, 20, 30
+
+print("x =", x)
+print("y =", y)
+print("z =", z)
