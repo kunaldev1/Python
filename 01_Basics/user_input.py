1a0deeb039023d1721a7483450497aee023a205a
Kunal Jha <jhakunal817@gmail.com>
2026-07-23 08:12:10 +0545

    Update user_input.py

--- 01_Basics/user_input.py
+++ 01_Basics/user_input.py
@@ -1,1 +1,19 @@
+# user_input.py
 
+# Taking users input
+
+name = input("Enter your name: ")   #String Value
+age = int(input("Enter your age: "))   #Integer Value
+height = float(input("Enter your height: "))   #Floating Value
+
+print("\n----- User Information -----")
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+print("\nHello", name + "!")
+print("Next year you will be", age + 1, "years old.")
+
+# Output:
+# Enter your name: Kunal
+# Enter your age: 17
