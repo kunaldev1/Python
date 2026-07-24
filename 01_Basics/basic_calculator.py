2531326eec8a17c7f7c643c7de0d55acc59118a9
Kunal Jha <jhakunal817@gmail.com>
2026-07-24 06:06:58 +0545

    Update basic_calculator.py

--- 01_Basics/basic_calculator.py
+++ 01_Basics/basic_calculator.py
@@ -1,1 +1,69 @@
+# basic_calculator.py
 
+print("===== Basic Calculator =====")
+
+# Get input from the user
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+# Perform calculation
+if operator == "+":     #if loop is used
+    result = num1 + num2
+    print("Result:", result)
+
+elif operator == "-":    #elif loop is used
+    result = num1 - num2
+    print("Result:", result)
+
+elif operator == "*":     #elif loop is used
+    result = num1 * num2
+    print("Result:", result)
+
+elif operator == "/":     #elif loop is used
+    if num2 != 0:         #Nested if loop is used(it means loop inside of loop)
+        result = num1 / num2
+        print("Result:", result)
+    else:                  #Nested loop is used 
+        print("Error: Division by zero is not allowed.")
+
+else:                       
+    print("Invalid operator! Please use +, -, *, or /.")
+
+# Output:
+# ===== Basic Calculator =====
+# Addition Operation:
+Enter first number: 15
+Enter operator (+, -, *, /): +
+Enter second number: 25
+Result: 40.0
+
+
+Enter first number: 50
+Enter operator (+, -, *, /): -
+Enter second number: 18
+Result: 32.0
+
+===== Basic Calculator =====
+Enter first number: 12
+Enter operator (+, -, *, /): *
+Enter second number: 8
+Result: 96.0
+
+===== Basic Calculator =====
+Enter first number: 45
+Enter operator (+, -, *, /): /
+Enter second number: 9
+Result: 5.0
+
+===== Basic Calculator =====
+Enter first number: 10
+Enter operator (+, -, *, /): /
+Enter second number: 0
+Error: Division by zero is not allowed.
+
+===== Basic Calculator =====
+Enter first number: 12
+Enter operator (+, -, *, /): %
+Enter second number: 5
+Invalid operator! Please use +, -, *, or /.
